9eb09a2e9030870bd773d0d62abdb831c5c63f83
Hyeonwoo Kang <kanglib14@gmail.com>
2018-06-07 17:27:03 +0900

    Revert "Constraint compiler WIP"
    This reverts commit 74d9a152aefb46cd9a77c49dbe7b6f0a49be77e2.

--- claripy/backends/z3_hook.py
+++ claripy/backends/z3_hook.py
@@ -1,7 +1,5 @@
 import z3
 
-from jit import JIT
-
 
 class Solver(z3.Solver):
     def __init__(self, *args, **kwargs):
@@ -9,8 +7,5 @@ class Solver(z3.Solver):
 
     def check(self):
         assertions = self.assertions()
-        a = assertions[len(assertions) - 1]
-        print(a.sexpr())
-        expr = JIT.parse_sexpr(a.sexpr())[0]
-        JIT().compile_expr(expr)
+        print assertions
         return z3.Solver.check(self)
